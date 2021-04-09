e2e2b228ed491e2fe9d44c637ca0cd570d2690d8
Wei Kang Wong <wwk97@hotmail.sg>
2021-04-10 05:05:41 +0800

    Optimised Sign Up

--- src/components/BizSignUp.vue
+++ src/components/BizSignUp.vue
@@ -1,66 +1,116 @@
 <template>
   <div>
     <app-header></app-header>
-    <img src="../assets/TopPic.png"/>
-    <div class='signup'>
-      <img id='logo' src="../assets/logo.png"/>
+    <img src="../assets/TopPic.png" />
+    <div class="signup">
+      <img id="logo" src="../assets/logo.png" />
       <h1>LET'S COLLAB!!</h1>
-      
+
       <form>
-        <input type="name" id="name" name="name" placeholder="NAME" v-model="form.name"><br><br>
-        <input type="email" id="email1" name="email" placeholder="BIZ EMAIL" v-model="form.email"><br><br>
-        <input type="number" id="mobile" name="mobile" placeholder= "CONTACT NUMBER" v-model="form.mobile"><br>
-        
+        <input
+          type="name"
+          id="name"
+          name="name"
+          placeholder="NAME"
+          v-model="form.name"
+          v-on:keyup.enter="register"
+        /><br /><br />
+        <input
+          type="email"
+          id="email1"
+          name="email"
+          placeholder="BIZ EMAIL"
+          v-model="form.email"
+          v-on:keyup.enter="register"
+        /><br /><br />
+        <input
+          type="number"
+          id="mobile"
+          name="mobile"
+          placeholder="CONTACT NUMBER"
+          v-model="form.mobile"
+          v-on:keyup.enter="register"
+        /><br />
+
         <label for="eat">EAT</label>
-        <input type="radio" name="radioBtn" id ="eat" value="eat" v-model="form.type">
-        
+        <input
+          type="radio"
+          name="radioBtn"
+          id="eat"
+          value="eat"
+          v-model="form.type"
+        />
+
         <label for="play">PLAY</label>
-        <input type="radio" name="radioBtn" id="play" value="play" v-model="form.type">
-        
+        <input
+          type="radio"
+          name="radioBtn"
+          id="play"
+          value="play"
+          v-model="form.type"
+        />
+
         <label for="shop">SHOP</label>
-        <input type="radio" name="radioBtn" id="shop" value="shop" v-model="form.type">
-        
-        <br>
-        <input type="button" id='submit' value="SIGN UP" v-on:click="register()">
-      </form>  
-      <router-link to="/" id='no' exact> NO, THANK YOU</router-link>
-    </div>   
+        <input
+          type="radio"
+          name="radioBtn"
+          id="shop"
+          value="shop"
+          v-model="form.type"
+        />
+
+        <br />
+        <input
+          type="button"
+          id="submit"
+          value="SIGN UP"
+          v-on:click="register"
+        />
+      </form>
+      <router-link to="/" id="no" exact> NO, THANK YOU</router-link>
+    </div>
   </div>
 </template>
 
 <script>
-  import Header from './Header.vue';
-  import db from "../firebase.js"
+import Header from "./Header.vue";
+import db from "../firebase.js";
 
-  export default {
-    data() {
-      return {
-        form: {
-          name: "",
-          email: "",
-          mobile:"",
-          type:""
-          
-        },
-      };
-    },
-
-    components: {
-      AppHeader:Header
-    },
-   
-    methods: {
-       register: function() {
-         if (this.form.name=="" || this.form.email=="" || this.form.mobile=="" || this.form.type=="") {
-          alert("Incomplete submission!");
-        } else {    
-          db.collection("pendingShops").add(this.form);
-          alert("Your submission has been recorded! We will get back to you soon! Have a nice day! :)");
-          this.$router.replace({ path: "/" });
-        }
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+        email: "",
+        mobile: "",
+        type: "",
       },
+    };
+  },
+
+  components: {
+    AppHeader: Header,
+  },
+
+  methods: {
+    register: function () {
+      if (
+        this.form.name == "" ||
+        this.form.email == "" ||
+        this.form.mobile == "" ||
+        this.form.type == ""
+      ) {
+        alert("Incomplete submission!");
+      } else {
+        db.collection("pendingShops").add(this.form);
+        alert(
+          "Your submission has been recorded! Kindly give us 3 working days to confirm your details! Have a nice day! :)"
+        );
+        this.$router.replace({ path: "/" });
+      }
     },
-  }
+  },
+};
 </script>
 
 <style scoped>
@@ -75,9 +125,9 @@ input {
 }
 
 #submit {
-  background-color: #D25A7E;
+  background-color: #d25a7e;
   width: auto;
-  color:white;
+  color: white;
   padding-left: 20px;
   padding-right: 20px;
   padding-top: 15px;
@@ -91,21 +141,20 @@ input {
 h1 {
   margin: 0pc;
   margin-bottom: 20px;
-  color:#D25A7E;
+  color: #d25a7e;
 }
 ::placeholder {
   color: #b86a9960;
   font-size: 10px;
-  padding:5px
+  padding: 5px;
 }
 .signup {
-  
   border: 1px solid rgb(95, 0, 32);
   border-radius: 10px;
   width: fit-content;
   margin-left: auto;
   margin-right: auto;
-  padding:20px;
+  padding: 20px;
   margin-top: 70px;
   margin-bottom: 70px;
 }
@@ -115,11 +164,13 @@ form {
 }
 
 label {
-  color:#D25A7E;
+  color: #d25a7e;
 }
-#eat, #play, #shop {
-  width:20px;
-  height:auto;
+#eat,
+#play,
+#shop {
+  width: 20px;
+  height: auto;
   margin-top: 20px;
   margin-bottom: 20px;
   margin-left: 5px;
@@ -127,15 +178,13 @@ label {
 
 #logo {
   width: 150px;
-  
 }
 
 #no {
   text-decoration-line: underline;
   font-size: 10px;
-  color: #A90065;
+  color: #a90065;
   margin: 50px;
   padding: 0px;
 }
-
 </style>
--- src/components/PastEvents.vue
+++ src/components/PastEvents.vue
@@ -1,30 +1,29 @@
 <template>
-
-<!-- without firebase*/ -->
-<div>
-    <Header></Header>
-    <h1>PAST EVENTS</h1>
+  <div>
     <table class="table">
       <tr>
         <th></th>
         <th>Item</th>
-        <th>Date</th>
+        <th>Date (DD/MM/YYYY)</th>
         <th>Quantity</th>
-        <th></th>
       </tr>
-      <tr v-for="item in past" v-bind:key="item.name">
-        <a v-bind:href="item.link">
-          <img v-bind:src="item.image"/>
-        </a>
-        <td>{{item.name}}</td>
+      <tr v-for="event in past" v-bind:key="event.index">
         <td>
-          {{item.date}}
+          <img :src="event.imageURL" />
         </td>
         <td>
-          {{item.quantity}}
+          {{ event.merchant_name }}
         </td>
+        <td>
+          {{ event.date.toDate().toLocaleDateString() }},
+          {{ event.date.toDate().getHours() }}:{{
+            event.date.toDate().getMinutes()
+          }}
+        </td>
+        <td>{{ event.adults }} x Adults, {{ event.children }} x Children</td>
       </tr>
     </table>
+    <p v-show="noPast">You have no past reservation</p>
   </div>
 </template>
 
@@ -34,44 +33,18 @@
 import Header from './Header.vue'
 
 export default {
-  components: {
-    Header: Header,
-  },
+  props: ["past"],
   data() {
     return {
-      past: [{
-                  name:'BBQ Place',
-                  date: '20.03.2021',
-                  quantity: '2',
-                  image:'https://withlocals-com-res.cloudinary.com/image/upload/w_750,h_424,c_fill,g_auto,q_auto,dpr_2.0,f_auto/0f88182574826cc51cdc182eecd1c5db'
-                  },
-                  {name:'Botany Workshop',
-                  date: '21.03.2021',
-                  quantity: '3',
-                  image:'https://sf1.mariefranceasia.com/wp-content/uploads/sites/7/2018/05/22218212_801883149982834_2264372406007038949_o-617x410.jpg'
-                  },
-                  {name:'Marui Sushi',
-                  date: '25.03.2021',
-                  quantity: '2',
-                  link: 'http://maruisushi4u.com/',
-                  image:'https://www.misstamchiak.com/wp-content/uploads/2018/03/38853624650_64b7bca360_h.jpg'
-                  },
-                  {name:"Sunday Folks",
-                  date: '10.03.2021',
-                  quantity: '2',
-                  link: 'https://www.facebook.com/sundayfolks.singapore',
-                  image:'https://media.timeout.com/images/101732859/1372/772/image.jpg'
-                  },
-                  {name:'Tombalek',
-                  date: '01.04.2021',
-                  quantity: '1',
-                  link: 'https://tombalek.com/',
-                  image:'https://zula.sg/wp-content/uploads/2020/10/xcraft-workshops-singapore-tombalek.jpg.pagespeed.ic.DBPg_Sbq6y.webp'
-                  }],
+      noPast: false,
     };
   },
-  methods: {         
-    
+  methods: {
+    checkPast: function () {
+      if (this.past.length == 0) {
+        this.noPast = true;
+      }
+    },
   },
   created() {
     
@@ -80,55 +53,40 @@ export default {
 </script>
 
 <style scoped>
-  h1 {
-    text-align: center;
-    color: #ed83a7;
-    font-size: 30px;
-    letter-spacing: 0.1em;
-    }
-
-  th {
-    vertical-align: center;
-    border-bottom:1px solid #403939;
-    margin: 20px;
-  }
-  td {
-    vertical-align: center;
-    border-bottom:1px solid #403939;
-  }
-
-  table {
-    width: 95%;
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    vertical-align: center;
-    font-size: 18px;
-    color: #403939;
-  }
-  hr {
-    color: #403939;
-    height: 1px;
-  }
-  img {
-    width: 200px;
-    height: 135px;
-    border-radius: 8px;
-    background-color: white;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    margin-bottom: 30px;
-    margin-top: 30px;
-  }
-  #deleteBtn {
-    background-color: #ED83A7;
-    font-size: 18px;
-    padding: 5px 15px;
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    color: #403939;
-    border: none;
-    cursor: pointer;
-    box-shadow:0 0 15px 4px rgba(0,0,0,0.06);
-    border-radius: 5px;
-  }
-
-
+th {
+  padding-bottom: 20px;
+  padding-top: 20px;
+  color: #ed83a7;
+}
+td {
+  white-space: nowrap;
+  overflow: hidden;
+}
+tr {
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.13);
+  border-radius: 10px;
+  background: #fff;
+}
+table {
+  width: 850px;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-size: 18px;
+  color: #403939;
+  padding-left: 0px;
+  table-layout: fixed;
+}
+hr {
+  color: #403939;
+  height: 1px;
+}
+img {
+  width: 100px;
+  height: 80px;
+  border-radius: 8px;
+  background-color: white;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.09);
+  margin-bottom: 30px;
+  margin-top: 30px;
+}
 </style>
 
