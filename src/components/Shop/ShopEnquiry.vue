7a2b9f325ffda40e06fdb0356706255cc6f5fc33
KyXuan229 <weeshaun229@gmail.com>
2021-04-14 14:55:50 +0800

    added enquiry tab for shops

---
+++ src/components/Shop/ShopEnquiry.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p> ADD SHIT HERE </p>
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+
+}
+</script>
+
+<style scoped>
+
+div {
+    margin-left: 20px;
+}
+</style>
