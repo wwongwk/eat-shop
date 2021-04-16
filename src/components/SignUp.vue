d981f5e8fcf27995ed5a7ba1c3db18d01990613b
yuzhen26 <65772624+yuzhen26@users.noreply.github.com>
2021-04-16 11:05:56 +0800

    strong pw

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,92 +1,184 @@
 <template>
   <div>
     <app-header></app-header>
-    <img src="../assets/TopPic.png"/>
-    <div class='signup'>
-      <img id='logo' src="../assets/logo.png"/>
+    <img src="../assets/TopPic.png" />
+    <div class="signup">
+      <img id="logo" src="../assets/logo.png" />
       <h1>JOIN US</h1>
       <form>
-        <input type="name" id="name" name="name" placeholder="USERNAME" v-model="form.name" v-on:keyup.enter="register"><br><br>
-        <input type="email" id="email1" name="email" placeholder="YOUR EMAIL" v-model="form.email" v-on:keyup.enter="register"><br><br>
-        <input type="password" id="password" name="password" placeholder= "PASSWORD" v-model="form.password" v-on:keyup.enter="register"><br><br>
-        <input type="tel" id="mobile" name="mobile" placeholder= "MOBILE" v-on:keyup="onlyNumberKey($event)"  v-model="form.mobile" v-on:keyup.enter="register"><br><br>
-        <input type="button" id='submit' value="SIGN UP" v-on:click="register">
-      </form>      
-      <router-link to="/" id='no' exact> NO, THANK YOU</router-link><br>
-      <router-link to="/bizsignup" id='biz' exact>FOR BUSINESS</router-link>
-   
-    </div>   
+        <input
+          type="name"
+          id="name"
+          name="name"
+          placeholder="USERNAME"
+          v-model="form.name"
+          v-on:keyup.enter="register"
+        /><br /><br />
+        <input
+          type="email"
+          id="email1"
+          name="email"
+          placeholder="YOUR EMAIL"
+          v-model="form.email"
+          v-on:keyup.enter="register"
+        /><br /><br />
+        <input
+          type="tel"
+          id="mobile"
+          name="mobile"
+          placeholder="MOBILE"
+          v-on:keyup="onlyNumberKey($event)"
+          v-model="form.mobile"
+          v-on:keyup.enter="register"
+        /><br /><br />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="PASSWORD"
+          v-model="form.password"
+          v-on:keyup.enter="register"
+        /><br /><br />
+        <input
+          type="password"
+          id="confirmpassword"
+          name="confirmpassword"
+          placeholder="CONFIRM PASSWORD"
+          v-model="form.confirmpassword"
+          v-on:keyup.enter="checkPassword, register"
+        /><br /><br />
+        
+        <input
+          type="button"
+          id="submit"
+          value="SIGN UP"
+          v-on:click="register"
+        />
+      </form>
+      <div id="lengthError" v-show="lengthErrorShown">
+          {{ lengthErrorMessage }}
+        </div>
+        <div id="lowercaseError" v-show="lowercaseErrorShown">
+          {{ lowercaseErrorMessage }}
+        </div>
+        <div id="uppercaseError" v-show="uppercaseErrorShown">
+          {{ uppercaseErrorMessage }}
+        </div>
+      <router-link to="/" id="no" exact> NO, THANK YOU</router-link><br />
+      <router-link to="/bizsignup" id="biz" exact>FOR BUSINESS</router-link>
+    </div>
   </div>
-
 </template>
 
 <script>
-  import Header from './Header.vue';
-  import database from '../firebase.js'
-  import firebase from "firebase/app"
-  import 'firebase/auth'
-  
+import Header from "./Header.vue";
+import database from "../firebase.js";
+import firebase from "firebase/app";
+import "firebase/auth";
 
-  export default {
-    data() {
-      return {
-        form: {
-          name: "",
-          email: "",
-          password: "",
-          mobile:""
-        },
-      };
-    },
-
-    components: {
-      AppHeader:Header
-    },
-   
-    methods: {
-
-      onlyNumberKey(evt) {
-          
-        // Only numbers allowed in mobile
-        var ASCIICode = (evt.which) ? evt.which : evt.keyCode
-        if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57)) {
-          alert("please key in only numbers")
-          this.form.mobile=""
-        }
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+        email: "",
+        password: "",
+        confirmpassword: "",
+        mobile: "",
       },
+      lengthErrorShown: false,
+      lowercaseErrorShown: false,
+      uppercaseErrorShown: false,
+      lengthErrorMessage: "",
+      lowercaseErrorMessage: "",
+      uppercaseErrorMessage: "",
+      strongPassword: true,
+    };
+  },
 
-      // user sign up with email and password,
-      //Conditions: Ensure that form is filled, ensure user is not registered
-      //Else: there will be an alert
-      register: function() {
-         if (this.form.name=="" || this.form.email=="" || this.form.password=="" || this.form.mobile=="") {
-          alert("Incomplete submission!");
-        } else {    
-          firebase
+  components: {
+    AppHeader: Header,
+  },
+
+  methods: {
+    onlyNumberKey(evt) {
+      // Only numbers allowed in mobile
+      var ASCIICode = evt.which ? evt.which : evt.keyCode;
+      if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57)) {
+        alert("please key in only numbers");
+        this.form.mobile = "";
+      }
+    },
+
+    checkPassword: function () {
+      var checked = false;
+      //check if password is strong -- contains at least 8 characters, a lowercase letter and an uppercase letter
+      if (this.form.password.length < 8) {
+        this.lengthErrorShown = true;
+        this.strongPassword = false;
+        checked = true;
+        this.lengthErrorMessage= "Your password is too short!"
+      }
+      if (this.form.password.toUpperCase() === this.form.password) {
+        this.lowercaseErrorShown = true;
+        this.strongPassword = false;
+        checked = true;
+        this.lowercaseErrorMessage= "Your password is missing a lowercase letter!"
+      }
+      if (this.form.password.toLowerCase() === this.form.password) {
+        this.uppercaseErrorShown = true;
+        this.strongPassword = false;
+        checked = true;
+        this.uppercaseErrorMessage= "Your password is missing an uppercase letter!"
+      }
+      if (!checked) {
+        this.strongPassword = true;
+      }
+    },
+
+    // user sign up with email and password,
+    //Conditions: Ensure that form is filled, ensure user is not registered and password is strong
+    //Else: there will be an alert
+    register: function () {
+      if (this.strongPassword === true) {
+      if (
+        this.form.name == "" ||
+        this.form.email == "" ||
+        this.form.password == "" ||
+        this.form.mobile == ""
+      ) {
+        alert("Incomplete submission!");
+      }
+      if (this.form.password !== this.form.confirmpassword) {
+        alert("Passwords are different! Check your password and try again.");
+      } else {
+        firebase
           .auth()
           .createUserWithEmailAndPassword(this.form.email, this.form.password)
           .then((data) => {
             database
-            .collection("users")
-            .doc(data.user.uid)
-            .set({
-              user_id: data.user.uid,
-              name: this.form.name,
-              mobile:this.form.mobile,
-              business:false,
-              favorites:{}
+              .collection("users")
+              .doc(data.user.uid)
+              .set({
+                user_id: data.user.uid,
+                name: this.form.name,
+                mobile: this.form.mobile,
+                business: false,
+                favorites: {},
               })
               .then(() => {
-                alert('Successfully signed up'); 
+                alert("Successfully signed up");
                 this.$router.replace({ path: "/" });
               });
-            }).catch((error) => {
+          })
+          .catch((error) => {
             alert(error.message);
           });
-        }
-      },
+      }
+      }
     },
-  }
+  },
+};
 </script>
 
 <style scoped>
@@ -105,9 +197,9 @@ input {
 }
 
 #submit {
-  background-color: #D25A7E;
+  background-color: #d25a7e;
   width: auto;
-  color:white;
+  color: white;
   padding-left: 20px;
   padding-right: 20px;
   padding-top: 13px;
@@ -122,40 +214,43 @@ input {
 h1 {
   margin: 0pc;
   margin-bottom: 20px;
-  color:#D25A7E;
+  color: #d25a7e;
 }
 ::placeholder {
   color: #b86a9960;
   font-size: 10px;
-  padding:5px
+  padding: 5px;
 }
 .signup {
-  
   border: 1px solid rgb(95, 0, 32);
   border-radius: 10px;
   width: fit-content;
   margin-left: auto;
   margin-right: auto;
-  padding:20px;
+  padding: 20px;
   margin-top: 70px;
   margin-bottom: 70px;
 }
 form {
   padding-left: 50px;
   padding-right: 50px;
-
 }
 #logo {
   width: 150px;
-  
 }
 
-#no, #biz {
+#lengthErrorMessage{
+  font-size: 20px;
+  text-align: center;
+  margin-top: 10px;
+}
+
+#no,
+#biz {
   text-decoration-line: underline;
   font-size: 10px;
-  color: #A90065;
+  color: #a90065;
   margin: 50px;
   padding: 0px;
 }
-
 </style>
