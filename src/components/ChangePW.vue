8dc6d41afb92d8af6839c04bf792e6a7352ff522
KyXuan229 <weeshaun229@gmail.com>
2021-04-19 23:52:48 +0800

    Updated ChangePW.vue to ensure strong password

--- src/components/ChangePW.vue
+++ src/components/ChangePW.vue
@@ -8,13 +8,31 @@
       <div id="content" class="flexbox">
         <form>
           <label for="new">*New Password:</label><br>
-          <input type="text" id="new" v-model="newpw"><br><br>
+          <input type="text" id="new" v-model="password"><br><br>
           <label for="re-enter">*Re-Enter New Password:</label><br>
-          <input type="text" id="re-enter" v-model="re_enter"><br><br>
+          <input type="text" id="re-enter" v-model="confirmpassword"><br><br>
+
+          <div id="errorDiv">
+            <div id="lengthError" v-show="lengthErrorShown">
+              {{ lengthErrorMessage }}
+            </div>
+            <div id="lowercaseError" v-show="lowercaseErrorShown">
+              {{ lowercaseErrorMessage }}
+            </div>
+            <div id="uppercaseError" v-show="uppercaseErrorShown">
+              {{ uppercaseErrorMessage }}
+            </div>
+            <div id="passwordMismatchError" v-show="passwordMismatchErrorShown">
+              {{ passwordMismatchMessage }}
+            </div>
+          </div>
+          <br>
           <input type="button" id='submit' value="CHANGE" v-on:click="change()">
         </form>
         <br>
       </div>
+
+      
     </div>
   </div>
 </template>
@@ -30,24 +48,98 @@
     },
     data() {
       return {  
-        newpw:"",
-        re_enter:""
+        password:"",
+        confirmpassword:"",
+
+        lengthErrorShown: true,
+        lowercaseErrorShown: true,
+        uppercaseErrorShown: true,
+        passwordMismatchErrorShown: true,
+        lengthErrorMessage: " - Password requires at least 8 characters",
+        lowercaseErrorMessage: "- Password requires a lowercase letter",
+        uppercaseErrorMessage: "- Password requires an uppercase letter",
+        passwordMismatchMessage: "- Password and Confirmation password need to match",
+
+        lengthCriteria: false,
+        lowercaseCriteria: false,
+        uppercaseCriteria: false,
+        matchCriteria: false,
+
+        strongPassword: false,
       }
     },
     methods: {
       change: function() {
-        if (this.newpw==this.re_enter) {
+        if (this.lengthCriteria === true && this.lowercaseCriteria === true && this.uppercaseCriteria === true && this.matchCriteria === true) {
           var user = firebase.auth().currentUser;
-          user.updatePassword(this.newpw).then(()=> {             
-            alert("Change successfully!");
+          user.updatePassword(this.password).then(()=> {             
+            alert("Password changed successfully!");
             this.$router.replace({ path: "/profile" });          
           }).catch(function(error) {
             console.log(error.message);
           });
         } else {
-          alert("Your passwords do not match!")
+          alert("Please check if your password fufill stated requirements.")
         }
       }
+    },
+    watch: {
+      password: function(pwd) {
+        this.password = pwd;
+        if (this.password.length >= 8) {
+          document.getElementById("lengthError").style.color = "green";
+          this.lengthCriteria = true;
+        }
+        if (this.password.length < 8) {
+          document.getElementById("lengthError").style.color = "red";
+          this.lengthCriteria = false;
+        }
+        if (this.password.toUpperCase() != this.password) { // password contains lowercase  QWERTY QWERTY
+          document.getElementById("lowercaseError").style.color = "green";
+          this.lowercaseCriteria = true;
+        }
+        if (this.password.toUpperCase() === this.password) { // password contains uppercase only  QWERTY QWERTY
+          document.getElementById("lowercaseError").style.color = "red";
+          this.lowercaseCriteria = false;
+        }
+        if (this.password.toLowerCase() != this.password) { // password contains uppercase  qwerty qwerty
+          document.getElementById("uppercaseError").style.color = "green";
+          this.uppercaseCriteria = true;
+        }
+        if (this.password.toLowerCase() === this.password) { // password contains lowercase only  qwerty qwerty
+          document.getElementById("uppercaseError").style.color = "red";
+          this.uppercaseCriteria = false;
+        }
+        if (this.password === this.confirmpassword){ // password same
+          document.getElementById("passwordMismatchError").style.color = "green";
+          this.matchCriteria = true;
+        }
+        if (this.password != this.confirmpassword){  // password mismatch
+          document.getElementById("passwordMismatchError").style.color = "red";
+          this.matchCriteria = false;
+
+        }
+        if (this.password == "" || this.confirmpassword == ""){  // password mismatch
+          document.getElementById("passwordMismatchError").style.color = "red";
+          this.matchCriteria = false;
+        }
+      },
+      confirmpassword: function(cfmPwd) {
+        this.confirmpassword = cfmPwd
+        if (this.password === this.confirmpassword){ // password same
+          document.getElementById("passwordMismatchError").style.color = "green";
+          this.matchCriteria = true;
+
+        }
+        if (this.password != this.confirmpassword){  // password mismatch
+          document.getElementById("passwordMismatchError").style.color = "red";
+          this.matchCriteria = false;
+        }
+        if (this.password == "" || this.confirmpassword == ""){  // password mismatch
+          document.getElementById("passwordMismatchError").style.color = "red";
+          this.matchCriteria = false;
+        }
+      }     
     },
   }
 </script>
@@ -116,5 +208,8 @@ form {
   width: 150px;
 }
 
-
+#errorDiv{
+  text-align: left;
+  color: red;
+}
 </style>
